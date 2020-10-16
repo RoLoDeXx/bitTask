01fd1752098745ae1540d24b36d0ab6738e2d949
Samarth Sharma <dexxrolo@gmail.com>
2020-10-16 11:25:44 +0530

    Updates

---
+++ precache-manifest.7f8ae3f11b8de558a48d0f8cad7ea91e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609f5bf07dcc98af178e6ac493ee01da",
+    "url": "/bitTask/index.html"
+  },
+  {
+    "revision": "3597ae2fddc56c13d3a2",
+    "url": "/bitTask/static/css/main.6c7c4af2.chunk.css"
+  },
+  {
+    "revision": "1fddf2f8e96c273c3488",
+    "url": "/bitTask/static/js/2.8780e5bd.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/bitTask/static/js/2.8780e5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3597ae2fddc56c13d3a2",
+    "url": "/bitTask/static/js/main.c54a62ab.chunk.js"
+  },
+  {
+    "revision": "ce5d8f6d22e975fc9dcf",
+    "url": "/bitTask/static/js/runtime-main.3b570e8a.js"
+  },
+  {
+    "revision": "7a621b3a944cc642152ddf9a3e24cec7",
+    "url": "/bitTask/static/media/bg.7a621b3a.jpeg"
+  },
+  {
+    "revision": "f12452a8d423c55534f2839350fd6f79",
+    "url": "/bitTask/static/media/logo.f12452a8.svg"
+  },
+  {
+    "revision": "8ea4ecdc0687d372c89f9770e9f73c98",
+    "url": "/bitTask/static/media/no-chat.8ea4ecdc.png"
+  }
+]);
